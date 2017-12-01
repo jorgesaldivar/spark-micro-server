1f9b40d241d1e4553699bfd41d2ad19ebe664abe
Jorge Saldivar <1724724+jorgesaldivar@users.noreply.github.com>
2017-11-30 23:06:31 -0600

    Updated shell command ann added error code 500 in case of failure

--- src/main/java/com/jorgesaldivar/SparkApp.java
+++ src/main/java/com/jorgesaldivar/SparkApp.java
@@ -2,11 +2,20 @@ package com.jorgesaldivar;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import spark.utils.StringUtils;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+/**
+ * Micro service using http://sparkjava.com/
+ * Retrieves the image name of the running docker containers in the host
+ *
+ * @author JorgeSaldivar
+ */
 
 public class SparkApp {
 
@@ -14,32 +23,56 @@ public class SparkApp {
 
     public static void main(String[] args) {
 
+        port(4455);
+
+        // Enabling HTTPS/SSL
+        //secure(keystoreFilePath, keystorePassword, truststoreFilePath, truststorePassword);
+
         get("/docker-running-images", (req, res) ->
-                getDockerRunningImages()
-
-
+                {
+                    res.type("application/json");
+                    String output = getDockerRunningImages();
+                    if (output.startsWith("There was an error")) {
+                        res.status(500);
+                        return "{\"message\":\"Internal Server Error\"}";
+                    } else if (output.split(" & ").length > 1)
+                        return "{\"images\":\"" + output + "\"}";
+                    else
+                        return "{\"image\":\"" + output + "\"}";
+                }
         );
     }
 
     private static String getDockerRunningImages() {
 
-        String result = "There was an error while trying to read the docker containers inside this instance";
+        String result = "There are no images running using docker containers";
 
         try {
 
-            Process process = Runtime.getRuntime().exec("docker ps | awk -F ' ' '{print $2}' | tail -n+2");
+            Process process = Runtime.getRuntime().exec(new String[]{
+                    "bash", "-c", "docker ps | awk -F ' ' '{print $2}' | tail -n+2"
+            });
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
             String line;
+            StringBuilder tempResult = new StringBuilder();
             while ((line = reader.readLine()) != null) {
-                result = String.join(" ", "Image:", line);
+                if (StringUtils.isBlank(tempResult)) {
+                    tempResult.append(line);
+                } else {
+                    tempResult.append(" & ").append(line);
+                }
             }
 
+            if (!StringUtils.isBlank(tempResult))
+                result = tempResult.toString();
+
         } catch (Exception e) {
+            result = "There was an error while trying to read the docker containers inside this instance";
             LOGGER.error("Error while trying to read the docker command", e);
         }
 
-        return result;
+        return result.trim();
 
     }
 
